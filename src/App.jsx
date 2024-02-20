99173143fd2ec9c8252f21f0e7de036e6438df8a
sudeep sharma <sudip777sharma@gmail.com>
2024-02-20 22:05:55 +0530

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ import MonacoEditorComponent from "./components/Editor";
 
 function App() {
   return (
-    <div className="flex items-center justify-center w-screen h-screen bg-slate-800 p-1 gap-1">
-      <div className="w-2/5 h-full rounded-lg">
+    <div className="flex md:flex-row flex-col items-center justify-center w-screen md:h-screen h-full bg-slate-800 p-1 gap-1">
+      <div className="md:w-2/5 w-full h-full rounded-lg">
         {/* <textarea className="w-full h-full bg-slate-700 p-2 rounded-lg outline-none"/> */}
         <MonacoEditorComponent />
       </div>
-      <div className="w-3/5 h-full border border-purple-600 rounded-lg">
+      <div className="md:w-3/5 w-full h-full border border-purple-600 rounded-lg">
         <TreeComponent />
       </div>
     </div>
